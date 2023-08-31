1997829ffe7b9d9771866903fe6c436053bba080
【﻿ｌｕｎａ】 <111466085+RimKaoutar@users.noreply.github.com>
2023-08-31 04:40:02 +0100

    Update website_status.py

--- website_status_checker/website_status.py
+++ website_status_checker/website_status.py
@@ -1,40 +1,65 @@
 import requests
 from pygame import mixer
 import time
+import sys
 
 """
 Customizable configuration variables
 
 url - URL of the site being monitored
-song - Path to song file played on success
-test_interval - Time interval between status checks in seconds
-song_looping_duration - Duration to loop the song playback in seconds
+song_file - Path to song file played on success
+check_interval - Time interval between status checks in seconds
+play_duration - Duration to loop the song playback in seconds
 """
-url = "https://tryhackme.com/" 
-song = "uptime_song.mp3"
-test_interval = 5
-song_looping_duration = 180
+url = "https://tryhackme.com/"
+song_file = "uptime_song.mp3"
+check_interval = 5
+play_duration = 180
 
-while True:
-  try:
-    result = requests.get(url)
-    if result.status_code == 200:  
-      mixer.init()
-      mixer.music.load(song)
-      print("Site is up!")
+def main():
 
-      # Play song for 3 minutes
-      start_time = time.time()
-      while time.time() - start_time < song_looping_duration:  
-        if not mixer.music.get_busy():
-          mixer.music.play()
+    try:
+        # Check URL status
+        response = request_site()
 
-      # Exit after 3 minutes
-      print("Exiting after 3 minutes of uptime")
-      break
+        # Handle response  
+        if response.status_code == 200:
+            print("Site is up!")  
+            play_notification()
+            sys.exit(0)
+        else:
+            print("Site is down")
+    except Exception as e:
+        print(f"Error checking site: {e}")
+        sys.exit(1)
 
-  except requests.ConnectionError:
-    print("Site is down...")
+    time.sleep(check_interval)
+    main()
 
-  # Keep testing every 5 seconds  
-  time.sleep(test_interval)
+def request_site():
+    print("Making request...")
+    try:
+        response = requests.get(url)
+        print(f"Status code: {response.status_code}")
+        return response
+
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+        sys.exit(1)
+
+def play_notification():
+    try:
+        mixer.init()
+        mixer.music.load(song_file)
+        print("Playing notification...")
+        mixer.music.play()
+
+        start_time = time.time()
+        while time.time() - start_time < play_duration:
+            if not mixer.music.get_busy():
+                mixer.music.play()
+    except Exception as e:
+        print(f"Error playing file: {e}")
+
+if __name__ == '__main__':
+    main()
